1b96b7a342f7499baab0a1aa89bc67208ddaef5a
bbalaji@123 <boddubalajinanda4356@gmail.com>
2025-11-20 18:59:29 +0530

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import AboutUs from './components/AboutUs';
-import './App.css';
 
 function App() {
   return (

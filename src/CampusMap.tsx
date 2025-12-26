006b4361cb534c9d4dba20caf39eea26304da09f
bbalaji@123 <boddubalajinanda4356@gmail.com>
2025-12-26 09:02:36 +0530

    error fixed

--- src/CampusMap.tsx
+++ src/CampusMap.tsx
@@ -23,9 +23,6 @@ const CampusMap: React.FC = () => {
       }}
     >
       {/* Floating Flower - Top Right */}
-
-      <div className="fixed -top-32 -right-32 md:-top-64 md:-right-64 pointer-events-none w-[300px] h-[300px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] opacity-25 z-1">
-        <FlowerComponent 
       <div className="fixed -top-32 -right-32 md:-top-64 md:-right-64 pointer-events-none w-[300px] h-[300px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] opacity-25 z-[1]">
         <FlowerComponent
           size="100%"
@@ -40,8 +37,6 @@ const CampusMap: React.FC = () => {
       </div>
 
       {/* Floating Flower - Bottom Left */}
-      <div className="fixed -bottom-32 -left-32 md:-bottom-64 md:-left-64 pointer-events-none w-[300px] h-[300px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] opacity-25 z-1">
-        <FlowerComponent 
       <div className="fixed -bottom-32 -left-32 md:-bottom-64 md:-left-64 pointer-events-none w-[300px] h-[300px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] opacity-25 z-[1]">
         <FlowerComponent
           size="100%"
